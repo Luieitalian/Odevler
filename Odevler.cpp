1f735155c0f6bef861d2d353b4ce8c2d1cb05ea8
Luieitalian <enderbud125@hotmail.com>
2022-10-23 03:37:18 +0300

    Odev1 tamam

--- Odevler.cpp
+++ Odevler.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <locale.h>
+using namespace std;
+
+enum gunler { pzt, sal, crs, per, cum, cmt, paz};
 
 int main(){
+    setlocale(LC_ALL, "Turkish");
 
+    enum gunler gun;
+    int secim;
 
+    cout << "Rakam giriniz: ";
+    cin >> secim;
+
+    gun = static_cast<gunler>(secim);
+
+    switch(gun){
+        case pzt:
+            cout << "Pazartesi";
+            break;
+        case sal:
+            cout << "Sali";
+            break;
+        case crs:
+            cout << "Çarşamba";
+            break;
+        case per:
+            cout << "Perşembe";
+            break;
+        case cum:
+            cout << "Cuma";
+            break;
+        case cmt:
+            cout << "Cumartesi";
+            break;
+        case paz:
+            cout << "Pazar";
+            break;
+        default:
+            cout << "Hata!";
+            break;
+    }
+    
     return 0;
 }
